704e9ee361595df2482a0abcf138fb0ee6372395
Dishant Sahu <dishant.sahu07@gmail.com>
2024-03-19 00:23:37 +0530

    fixed: route

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -9,8 +9,14 @@ const SideBar = () => {
   return (
     <div className=" w-40 md:w-52 bg-blue-400 h-screen flex flex-col font-bold gap-3 ">
       <button
-        onClick={() => navigate("java")}
+        onClick={() => navigate("")}
         className=" flex mt-2 justify-center "
+      >
+        Home
+      </button>
+      <button
+        onClick={() => navigate("java")}
+        className=" flex  justify-center "
       >
         Java
       </button>
